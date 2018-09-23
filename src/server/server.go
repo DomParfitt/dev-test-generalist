cc64a4f0001efb291da4ee29d215f2e77fa5784d
Dom Parfitt <dom.parfitt@gmail.com>
2018-09-23 17:46:55 +0100

    Added struct for Bike JSON and simple handling to serve req for a bike

--- src/server/server.go
+++ src/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -13,6 +14,7 @@ func Serve(port string) {
 	router.HandleFunc("/getAllBikes", getAllBikesHandler)
 	router.HandleFunc("/addBike", addBikeHandler)
 
+	fmt.Printf("Listening on port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
 
@@ -20,7 +22,26 @@ func getBikeHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	bikeID := vars["bikeID"]
 
-	fmt.Fprintf(w, "Received a GET request for bike with ID %s.", bikeID)
+	fmt.Printf("Received a GET request for bike with ID %s.\n", bikeID)
+
+	bike := &bike{
+		BikeID:      1,
+		Name:        "Dummy Bike",
+		Description: "Placeholder data for a non-existant bike",
+		Price:       "1,000,000",
+	}
+
+	json, err := json.Marshal(bike)
+
+	if err != nil {
+		fmt.Fprintf(w, "Could not retrieve bike with ID %s", bikeID)
+		return
+	}
+
+	fmt.Printf("Returning result %s\n", json)
+
+	fmt.Fprintf(w, "%s", json)
+
 }
 
 func getAllBikesHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +50,11 @@ func getAllBikesHandler(w http.ResponseWriter, r *http.Request) {
 
 func addBikeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Received a POST request to add a new bike.")
+}
+
+type bike struct {
+	BikeID      int
+	Name        string
+	Description string
+	Price       string
 }
