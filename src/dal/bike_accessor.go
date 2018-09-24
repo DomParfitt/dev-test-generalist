9245b1a50ea60783049d323153108b04e26c14eb
Dom Parfitt <dom.parfitt@gmail.com>
2018-09-24 21:02:02 +0100

    Updated interface function names

--- src/dal/bike_accessor.go
+++ src/dal/bike_accessor.go
@@ -7,7 +7,7 @@ import (
 //BikeAccessor defines an interface for types that can
 //access a collection of Bike objects, e.g. in a database
 type BikeAccessor interface {
-	getBike(id int) (common.Bike, error)
-	getAllBikes() []common.Bike
-	addBike(bike common.Bike)
+	GetBike(id int) (common.Bike, error)
+	GetAllBikes() []common.Bike
+	AddBike(bike common.Bike)
 }
