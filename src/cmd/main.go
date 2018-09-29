d694fee51f337315039dfe1fb47a494912acbdb0
Dom Parfitt <dom.parfitt@gmail.com>
2018-09-29 17:59:08 +0100

    Added some cmd line arg handling

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"github.com/DomParfitt/dev-test-generalist/src/dal/bike"
 	"github.com/DomParfitt/dev-test-generalist/src/server"
+	"os"
 )
 
 func main() {
-	server.Serve("8080")
+	args := os.Args[1:]
+
+	if len(args) < 2 {
+		fmt.Println("Unexpected number of arguments. Expected 1 - MongoDB URL")
+		return
+	}
+	url := args[0]
+	port := args[1]
+
+	bikeAccessor, err := bike.New(url)
+	if err != nil {
+		fmt.Printf("Could not connect to MongoDB instance at URL: %s", url)
+	}
+
+	server := server.New(bikeAccessor)
+	server.Serve(port)
+}
+
+func printUsage() {
+	fmt.Println("Usage")
+	fmt.Println("\tURL - URL of the DB instance")
+	fmt.Println("\tPort - Port to serve the API on")
+
 }
