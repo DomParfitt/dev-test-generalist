ea41ee114c66be2211d21cf36735b3a1b0434e31
Dom Parfitt <dom.parfitt@gmail.com>
2018-09-29 17:58:48 +0100

    Updated server to struct holding DB accessor

--- src/server/server.go
+++ src/server/server.go
@@ -4,40 +4,64 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/DomParfitt/dev-test-generalist/src/common"
+	"github.com/DomParfitt/dev-test-generalist/src/dal/bike"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+type Server struct {
+	bikeAccessor bike.Accessor
+}
+
+//New server
+func New(bikeAccessor bike.Accessor) *Server {
+	return &Server{bikeAccessor: bikeAccessor}
+}
+
 //Serve the API on the given port
-func Serve(port string) {
+func (s *Server) Serve(port string) {
 	router := mux.NewRouter()
-	router.HandleFunc("/getBike/{bikeID}", getBikeHandler)
-	router.HandleFunc("/getAllBikes", getAllBikesHandler)
-	router.HandleFunc("/addBike", addBikeHandler)
+	router.HandleFunc("/getBike/{bikeID}", s.getBikeHandler)
+	router.HandleFunc("/getAllBikes", s.getAllBikesHandler)
+	router.HandleFunc("/addBike", s.addBikeHandler)
 
 	fmt.Printf("Listening on port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
 
 //Handler for getting a single bike by its ID
-func getBikeHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) getBikeHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	bikeID := vars["bikeID"]
 
-	fmt.Printf("Received a GET request for bike with ID %s.\n", bikeID)
+	bikeID, err := strconv.Atoi(vars["bikeID"])
 
-	bike := &common.Bike{
-		BikeID:      1,
-		Name:        "Dummy Bike",
-		Description: "Placeholder data for a non-existant bike",
-		Price:       "1,000,000",
+	if err != nil {
+		fmt.Fprint(w, "Please provide a numeric ID for a bike")
+		return
+	}
+
+	fmt.Printf("Received a GET request for bike with ID %d.\n", bikeID)
+
+	// bike := &common.Bike{
+	// 	BikeID:      1,
+	// 	Name:        "Dummy Bike",
+	// 	Description: "Placeholder data for a non-existant bike",
+	// 	Price:       "1,000,000",
+	// }
+
+	bike, err := s.bikeAccessor.GetBike(bikeID)
+
+	if err != nil {
+		fmt.Fprintf(w, "Could not retrieve bike with ID %d", bikeID)
+		return
 	}
 
 	json, err := json.Marshal(bike)
 
 	if err != nil {
-		fmt.Fprintf(w, "Could not retrieve bike with ID %s", bikeID)
+		fmt.Fprintf(w, "Could not retrieve bike with ID %d", bikeID)
 		return
 	}
 
@@ -48,7 +72,7 @@ func getBikeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 //Handler for getting all bikes
-func getAllBikesHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) getAllBikesHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Received a GET request for all bikes.\n")
 
 	bikes := []common.Bike{}
@@ -77,6 +101,6 @@ func getAllBikesHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 //Handler for adding a new bike to the collection
-func addBikeHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) addBikeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Received a POST request to add a new bike.")
 }
