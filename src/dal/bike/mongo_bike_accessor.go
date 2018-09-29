8c37c78c6b660ada940984b887dea51af79329c2
Dom Parfitt <dom.parfitt@gmail.com>
2018-09-29 18:14:36 +0100

    Added implementation for getAllBikes

--- src/dal/bike/mongo_bike_accessor.go
+++ src/dal/bike/mongo_bike_accessor.go
@@ -30,6 +30,7 @@ func New(url string) (MongoBikeAccessor, error) {
 func (m MongoBikeAccessor) GetBike(id int) (common.Bike, error) {
 	bike := common.Bike{}
 	err := m.session.DB("test").C("bike").Find(bson.M{"bikeId": id}).One(&bike)
+	fmt.Printf("%v\n", bike)
 
 	if err != nil {
 		fmt.Printf("%s\n", err.Error())
@@ -42,6 +43,12 @@ func (m MongoBikeAccessor) GetBike(id int) (common.Bike, error) {
 //GetAllBikes from the connceted MongoDB database
 func (m MongoBikeAccessor) GetAllBikes() []common.Bike {
 	bikes := []common.Bike{}
+	err := m.session.DB("test").C("bike").Find(bson.M{}).All(&bikes)
+
+	if err != nil {
+		return []common.Bike{}
+	}
+
 	return bikes
 }
 
