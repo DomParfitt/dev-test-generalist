4fa77a665a533d8c690c92f6d23f84f65729e693
Dom Parfitt <dom.parfitt@gmail.com>
2018-09-30 17:04:00 +0100

    Updated args

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -10,26 +10,41 @@ import (
 func main() {
 	args := os.Args[1:]
 
-	if len(args) < 2 {
-		fmt.Println("Unexpected number of arguments. Expected 1 - MongoDB URL")
+	//If we don't have all the required args then exit
+	if len(args) < 4 {
+		fmt.Printf("Not enough arguments. Expected 4 but only %d provided.\n", len(args))
+		printUsage()
 		return
 	}
-	url := args[0]
-	port := args[1]
 
-	bikeAccessor, err := bike.New(url)
+	//Get the args into named vars
+	url := args[0]
+	db := args[1]
+	collection := args[2]
+	port := args[3]
+
+	//Create the DAL
+	bikeAccessor, err := bike.New(url, db, collection)
+
+	//If we can't connect to URL, DB, Collection combo then exit
 	if err != nil {
-		fmt.Printf("Could not connect to MongoDB instance at URL: %s", url)
+		fmt.Printf("Could not connect to MongoDB instance at URL: %s. Reason: %s.\n", url, err.Error())
+		return
 	}
+	//Defer closing of connection
 	defer bikeAccessor.Close()
 
+	//Create and start API server
 	server := server.New(bikeAccessor)
 	server.Serve(port)
 }
 
+//Print the usage of this utility
 func printUsage() {
-	fmt.Println("Usage")
-	fmt.Println("\tURL - URL of the DB instance")
-	fmt.Println("\tPort - Port to serve the API on")
+	fmt.Println("Usage: main url db_name collection_name port")
+	fmt.Println("\turl             - URL of the DB instance")
+	fmt.Println("\tdb_name         - Name of the DB")
+	fmt.Println("\tcollection_name - Name of the collection in the given DB")
+	fmt.Println("\tport            - Port to serve the API on")
 
 }
